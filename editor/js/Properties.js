1cb638bb0bee03d789da75ecb19dc310a56697f2
Mr.doob <info@mrdoob.com>
2013-11-06 20:10:51 +0900

    Properties panel starting to work. See #3.

--- editor/js/Properties.js
+++ editor/js/Properties.js
@@ -2,6 +2,69 @@ var Properties = function ( editor ) {
 
 	var container = new UI.Panel();
 
+	// signals
+
+	var signals = editor.signals;
+
+	editor.signals.elementSelected.add( function ( element ) {
+
+		console.log( element );
+
+		container.clear();
+
+		var parameters = element.parameters;
+
+		for ( var key in parameters ) {
+
+			var parameter = parameters[ key ];
+
+			var parameterRow = new UI.Panel();
+			parameterRow.add( new UI.Text( key ).setWidth( '90px' ) );
+
+			switch ( typeof( parameter ) ) {
+
+				case 'number':
+
+					parameterRow.add(
+
+						new UI.Number()
+							.setValue( parameter )
+							.setWidth( '150px' )
+							.onChange( function () {
+
+								element.parameters[ key ] = this.getValue();
+
+							} )
+
+					);
+
+					break;
+
+				case 'string':
+
+					parameterRow.add(
+
+						new UI.Input()
+							.setValue( parameter )
+							.setWidth( '150px' )
+							.onChange( function () {
+
+								element.parameters[ key ] = this.getValue();
+
+							} )
+
+					);
+
+					break;
+
+			}
+
+			container.add( parameterRow );
+
+		};
+
+	} );
+
 	return container;
 
 }
