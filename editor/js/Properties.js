559477aecaf2d0589fa90788342289897f9a6057
fernandojsg <fernandojsg@gmail.com>
2013-11-06 17:07:32 +0100

    Added vector2 and vector3 components

--- editor/js/Properties.js
+++ editor/js/Properties.js
@@ -8,8 +8,6 @@ var Properties = function ( editor ) {
 
 	editor.signals.elementSelected.add( function ( element ) {
 
-		console.log( element );
-
 		container.clear();
 
 		var parameters = element.module.parameters.input;
@@ -50,6 +48,69 @@ var Properties = function ( editor ) {
 						} );
 
 					parameterRow.add( parameterValue );
+
+				} else if ( parameter instanceof FRAME.ModuleParameter.Vector2 ) {
+
+					var vectorX = new UI.Number()
+						.setValue( element.parameters[ key ][1] )
+						.setWidth( '50px' )
+						.onChange( function () {
+
+							console.log(element.parameters[ key ],this.getValue());
+							element.parameters[ key ][0] = this.getValue();
+							signals.timelineElementChanged.dispatch( element );
+
+						} );
+				
+					var vectorY = new UI.Number()
+						.setValue( element.parameters[ key ][1] )
+						.setWidth( '50px' )
+						.onChange( function () {
+
+							element.parameters[ key ][1] = this.getValue();
+							signals.timelineElementChanged.dispatch( element );
+
+						} );
+				
+					parameterRow.add( vectorX );
+					parameterRow.add( vectorY );
+
+				} else if ( parameter instanceof FRAME.ModuleParameter.Vector3 ) {
+
+					var vectorX = new UI.Number()
+						.setValue( element.parameters[ key ][1] )
+						.setWidth( '50px' )
+						.onChange( function () {
+
+							console.log(element.parameters[ key ],this.getValue());
+							element.parameters[ key ][0] = this.getValue();
+							signals.timelineElementChanged.dispatch( element );
+
+						} );
+				
+					var vectorY = new UI.Number()
+						.setValue( element.parameters[ key ][1] )
+						.setWidth( '50px' )
+						.onChange( function () {
+
+							element.parameters[ key ][1] = this.getValue();
+							signals.timelineElementChanged.dispatch( element );
+
+						} );
+
+					var vectorZ = new UI.Number()
+						.setValue( element.parameters[ key ][2] )
+						.setWidth( '50px' )
+						.onChange( function () {
+
+							element.parameters[ key ][2] = this.getValue();
+							signals.timelineElementChanged.dispatch( element );
+
+						} );
+				
+					parameterRow.add( vectorX );
+					parameterRow.add( vectorY );
+					parameterRow.add( vectorZ );
 
 				} else if ( parameter instanceof FRAME.ModuleParameter.String ) {
 
