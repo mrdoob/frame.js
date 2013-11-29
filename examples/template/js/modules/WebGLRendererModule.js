5a7d986700f7f1d65c0b1b4736f37bb932837ecd
Mr.doob <info@mrdoob.com>
2013-11-29 15:11:14 +0800

    Template demo needs the renderer to have the final size.

--- examples/template/js/modules/WebGLRendererModule.js
+++ examples/template/js/modules/WebGLRendererModule.js
@@ -6,6 +6,7 @@
 	dom.style.height = '100%';
 
 	var renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( config.width, config.height );
 	renderer.autoClear = false;
 	renderer.domElement.style.position = 'absolute';
 	dom.appendChild( renderer.domElement );
@@ -17,9 +18,10 @@
 		var width = config.width * scale;
 		var height = config.height * scale;
 
-		renderer.setSize( width, height );
 		renderer.domElement.style.left = ( dom.offsetWidth - width ) / 2 + 'px';
 		renderer.domElement.style.top = ( dom.offsetHeight - height ) / 2 + 'px';
+		renderer.domElement.style.width = width + 'px';
+		renderer.domElement.style.height = height + 'px';
 
 	};
 
