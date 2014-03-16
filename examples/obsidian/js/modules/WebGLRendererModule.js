874ba0ae8a22b6b946d83120607899f3ddcfc225
Mr.doob <info@mrdoob.com>
2014-03-15 18:17:59 -0600

    Fixed Obsidian's WebGLRendererModule.

--- examples/obsidian/js/modules/WebGLRendererModule.js
+++ examples/obsidian/js/modules/WebGLRendererModule.js
@@ -4,6 +4,7 @@
 	dom.style.position = 'absolute';
 	dom.style.width = '100%';
 	dom.style.height = '100%';
+	config.dom.appendChild( dom );
 
 	var renderer = new THREE.WebGLRenderer( { antialias: true, preserveDrawingBuffer: true } );
 	renderer.autoClear = false;
@@ -18,10 +19,15 @@
 		var height = config.height * scale;
 
 		renderer.setSize( width, height );
+
 		renderer.domElement.style.left = ( dom.offsetWidth - width ) / 2 + 'px';
 		renderer.domElement.style.top = ( dom.offsetHeight - height ) / 2 + 'px';
 
 	};
+
+	window.addEventListener( 'resize', onWindowResize );
+
+	onWindowResize();
 
 	config.renderer = renderer;
 
@@ -31,27 +37,9 @@
 
 		parameters: {
 
-			dom: new FRAME.ModuleParameter.DOM( 'DOM', null ),
 			clearColor: new FRAME.ModuleParameter.Boolean( 'Clear color', true ),
 			clearDepth: new FRAME.ModuleParameter.Boolean( 'Clear depth', true ),
 			clearStencil: new FRAME.ModuleParameter.Boolean( 'Clear stencil', true )
-
-		},
-
-		init: function ( parameters ) {
-
-			if ( parameters.dom !== null ) {
-
-				parameters.dom.appendChild( dom );
-				parameters.dom = null; // TODO: Fix hack
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize );
-
-				onWindowResize();
-
-			}
 
 		},
 
