e8582c42f69e562ee2b8c67b0d631e0d0af2c21a
Mr.doob <info@mrdoob.com>
2017-04-12 17:22:47 -0700

    Removed build and utils folder.

--- utils/builder.py
+++
@@ -1,9 +0,0 @@
-import os
-
-source = '../src/Frame.js'
-output = '../build/frame.min.js'
-
-os.system('java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + source + ' --js_output_file ' + output)
-
-with open(output,'r') as f: text = f.read()
-with open(output,'w') as f: f.write("// frame.js - http://github.com/mrdoob/frame.js\n" + text)
