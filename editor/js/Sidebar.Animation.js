2267c811f0f44fc936f12ffb3c2e3642f376fd63
Mr.doob <info@mrdoob.com>
2018-07-18 14:46:12 +0100

    Sidebar.Animation: Vars renaming.

--- editor/js/Sidebar.Animation.js
+++ editor/js/Sidebar.Animation.js
@@ -20,7 +20,6 @@ Sidebar.Animation = function ( editor ) {
 
 		var parameterRow = new UI.Row();
 		parameterRow.add( new UI.Text( parameter.name ).setWidth( '90px' ) );
-		var animation = editor.selected;
 
 		if ( parameter instanceof FRAME.Parameters.Boolean ) {
 
@@ -29,7 +28,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -46,7 +45,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -63,7 +62,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -79,7 +78,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value[ 0 ] = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -89,7 +88,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value[ 1 ] = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -104,7 +103,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value[ 0 ] = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -114,7 +113,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value[ 1 ] = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -124,7 +123,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value[ 2 ] = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -140,7 +139,7 @@ Sidebar.Animation = function ( editor ) {
 				.onKeyUp( function () {
 
 					parameter.value = this.getValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -154,7 +153,7 @@ Sidebar.Animation = function ( editor ) {
 				.onChange( function () {
 
 					parameter.value = this.getHexValue();
-					signals.animationModified.dispatch( animation );
+					signals.animationModified.dispatch( selected );
 
 				} );
 
@@ -174,19 +173,17 @@ Sidebar.Animation = function ( editor ) {
 
 		values = {};
 
-		var animation = selected;
-
 		// Name
 
 		var row = new UI.Row();
 		row.add( new UI.Text( 'Name' ).setWidth( '90px' ) );
 		container.add( row );
 
-		var animationName = new UI.Input( animation.name )
+		var animationName = new UI.Input( selected.name )
 		animationName.onChange( function () {
 
-			animation.name = this.getValue();
-			signals.animationRenamed.dispatch( animation );
+			selected.name = this.getValue();
+			signals.animationRenamed.dispatch( selected );
 
 		} );
 		row.add( animationName );
@@ -197,20 +194,20 @@ Sidebar.Animation = function ( editor ) {
 		row.add( new UI.Text( 'Time' ).setWidth( '90px' ) );
 		container.add( row );
 
-		var animationStart = new UI.Number( animation.start ).setWidth( '80px' );
+		var animationStart = new UI.Number( selected.start ).setWidth( '80px' );
 		animationStart.onChange( function () {
 
-			animation.start = this.getValue();
-			signals.animationModified.dispatch( animation );
+			selected.start = this.getValue();
+			signals.animationModified.dispatch( selected );
 
 		} );
 		row.add( animationStart );
 
-		var animationEnd = new UI.Number( animation.end ).setWidth( '80px' );
+		var animationEnd = new UI.Number( selected.end ).setWidth( '80px' );
 		animationEnd.onChange( function () {
 
-			animation.end = this.getValue();
-			signals.animationModified.dispatch( animation );
+			selected.end = this.getValue();
+			signals.animationModified.dispatch( selected );
 
 		} );
 		row.add( animationEnd );
@@ -221,11 +218,11 @@ Sidebar.Animation = function ( editor ) {
 		row.add( new UI.Text( 'Layer' ).setWidth( '90px' ) );
 		container.add( row );
 
-		var animationLayer = new UI.Integer( animation.layer ).setWidth( '80px' );
+		var animationLayer = new UI.Integer( selected.layer ).setWidth( '80px' );
 		animationLayer.onChange( function () {
 
-			animation.layer = this.getValue();
-			signals.animationModified.dispatch( animation );
+			selected.layer = this.getValue();
+			signals.animationModified.dispatch( selected );
 
 		} );
 		row.add( animationLayer );
@@ -236,11 +233,11 @@ Sidebar.Animation = function ( editor ) {
 		row.add( new UI.Text( 'Enabled' ).setWidth( '90px' ) );
 		container.add( row );
 
-		var animationEnabled = new UI.Checkbox( animation.enabled )
+		var animationEnabled = new UI.Checkbox( selected.enabled )
 		animationEnabled.onChange( function () {
 
-			animation.enabled = this.getValue();
-			signals.animationModified.dispatch( animation );
+			selected.enabled = this.getValue();
+			signals.animationModified.dispatch( selected );
 
 		} );
 		row.add( animationEnabled );
@@ -265,13 +262,13 @@ Sidebar.Animation = function ( editor ) {
 		}
 
 		var effectsSelect = new UI.Select().setWidth( '130px' );
-		effectsSelect.setOptions( options ).setValue( effects.indexOf( animation.effect ) );
+		effectsSelect.setOptions( options ).setValue( effects.indexOf( selected.effect ) );
 		effectsSelect.onChange( function () {
 
 			editor.timeline.reset();
-			animation.effect = editor.effects[ this.getValue() ];
+			selected.effect = editor.effects[ this.getValue() ];
 
-			signals.animationModified.dispatch( animation );
+			signals.animationModified.dispatch( selected );
 
 			build();
 
@@ -281,7 +278,7 @@ Sidebar.Animation = function ( editor ) {
 		var edit = new UI.Button( 'EDIT' ).setMarginLeft( '8px' );
 		edit.onClick( function () {
 
-			editor.selectEffect( animation.effect );
+			editor.selectEffect( selected.effect );
 
 		} );
 		row.add( edit );
@@ -291,18 +288,18 @@ Sidebar.Animation = function ( editor ) {
 		row.add( new UI.Text( 'Name' ).setWidth( '90px' ) );
 		container.add( row );
 
-		var effectName = new UI.Input( animation.effect.name );
+		var effectName = new UI.Input( selected.effect.name );
 		effectName.onChange( function () {
 
-			animation.effect.name = this.getValue();
-			signals.effectRenamed.dispatch( animation.effect );
+			selected.effect.name = this.getValue();
+			signals.effectRenamed.dispatch( selected.effect );
 
 		} );
 		row.add( effectName );
 
 		//
 
-		var parameters = animation.effect.program.parameters;
+		var parameters = selected.effect.program.parameters;
 
 		for ( var key in parameters ) {
 
@@ -336,11 +333,9 @@ Sidebar.Animation = function ( editor ) {
 
 		if ( selected !== null ) {
 
-			var animation = selected;
-
 			for ( var key in values ) {
 
-				values[ key ].setValue( animation.module.parameters[ key ].value );
+				values[ key ].setValue( selected.module.parameters[ key ].value );
 
 			}
 
