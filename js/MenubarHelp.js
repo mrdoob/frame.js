1377cbc2f27bd91d8b83071213c08ff0b81e7f1a
Mr.doob <info@mrdoob.com>
2020-07-10 06:37:45 +0900

    Update MenubarHelp.js

--- js/MenubarHelp.js
+++ js/MenubarHelp.js
@@ -23,15 +23,7 @@ function MenubarHelp( editor ) {
 	var option = new UI.Panel();
 	option.setClass( 'option' );
 	option.setTextContent( 'Source code' );
-	option.onClick( function () { window.open( 'https://github.com/mrdoob/frame.js/tree/master/editor', '_blank' ) } );
-	options.add( option );
-
-	// about
-
-	var option = new UI.Panel();
-	option.setClass( 'option' );
-	option.setTextContent( 'About' );
-	option.onClick( function () { window.open( 'http://github.com/mrdoob/frame.js/', '_blank' ) } );
+	option.onClick( function () { window.open( 'https://github.com/mrdoob/frame.js/', '_blank' ) } );
 	options.add( option );
 
 	return container;
